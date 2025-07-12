9aa4535a9c4262de12129a4353bda65c1b2fad02
Nicola Mignoni <nicomignoni@users.noreply.github.com>
2025-07-12 20:14:13 +0200

    Pruned some comments, add julia-lsp

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -117,18 +117,14 @@ require("lazy").setup({
 	{
 	    'neovim/nvim-lspconfig',
 	    dependencies = {
-	    -- Automatically install LSPs and related tools to stdpath for Neovim
-	    -- Mason must be loaded before its dependents so we need to set it up here.
-	    -- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
 		{ 'mason-org/mason.nvim', opts = {} },
 		'mason-org/mason-lspconfig.nvim',
 		'WhoIsSethDaniel/mason-tool-installer.nvim',
 		'saghen/blink.cmp',
 	    },
 	    config = function()
-		-- Setup mason-lspconfig
 		require('mason-lspconfig').setup({
-		    ensure_installed = { 'pyright', 'lua_ls', },
+		    ensure_installed = { 'pyright', 'lua_ls', 'julia-lsp' },
 		})
 	    end
     	},
@@ -138,33 +134,17 @@ require("lazy").setup({
 	    'saghen/blink.cmp',
 	    version = '1.*',
 	    opts = {
-		-- All presets have the following mappings:
-	      	-- C-space: Open menu or open docs if already open
-	      	-- C-n/C-p or Up/Down: Select next/previous item
-	      	-- C-e: Hide menu
-	      	-- C-k: Toggle signature help (if signature.enabled = true)
-	      	--
-	      	-- See :h blink-cmp-config-keymap for defining your own keymap
+		-- Press enter to confirm
 	      	keymap = { preset = 'enter' },
-
 	      	appearance = {
 	      	  nerd_font_variant = 'normal'
 	      	},
-
 	      	-- (Default) Only show the documentation popup when manually triggered
 	      	completion = { documentation = { auto_show = false } },
-
-	      	-- Default list of enabled providers defined so that you can extend it
-	      	-- elsewhere in your config, without redefining it, due to `opts_extend`
 	      	sources = {
 	      	  default = { 'lsp', 'path', 'snippets', 'buffer' },
 	      	},
-
 	      	-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-	      	-- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-	      	-- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-	      	--
-	      	-- See the fuzzy documentation for more information
 		fuzzy = { implementation = "prefer_rust_with_warning" },
 		signature = { enabled = true },
 	    },
