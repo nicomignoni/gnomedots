2125851fcf5503b22e943e73d135dcb9dc4a0d8e
Nicola Mignoni <nicomignoni@users.noreply.github.com>
2025-07-12 15:01:47 +0200

    Add markdown preview and LSP config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -113,53 +113,60 @@ require("lazy").setup({
 	    end
 	},
 	
-	-- Mason
+	-- Main LSP Configuration
 	{
-	    "mason-org/mason.nvim",
-	    opts = {
-	        ui = {
-	            icons = {
-	                package_installed = "✓",
-	                package_pending = "➜",
-	                package_uninstalled = "✗"
-	            }
-	        }
-	    }
-	},
+	    'neovim/nvim-lspconfig',
+	    dependencies = {
+	    -- Automatically install LSPs and related tools to stdpath for Neovim
+	    -- Mason must be loaded before its dependents so we need to set it up here.
+	    -- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
+		{ 'mason-org/mason.nvim', opts = {} },
+		'mason-org/mason-lspconfig.nvim',
+		'WhoIsSethDaniel/mason-tool-installer.nvim',
+		'saghen/blink.cmp',
+	    },
+	    config = function()
+		-- Setup mason-lspconfig
+		require('mason-lspconfig').setup({
+		    ensure_installed = { 'pyright', 'lua_ls', },
+		})
+	    end
+    	},
 
 	-- Autocompletion
 	{
 	    'saghen/blink.cmp',
 	    version = '1.*',
 	    opts = {
-	      -- All presets have the following mappings:
-	      -- C-space: Open menu or open docs if already open
-	      -- C-n/C-p or Up/Down: Select next/previous item
-	      -- C-e: Hide menu
-	      -- C-k: Toggle signature help (if signature.enabled = true)
-	      --
-	      -- See :h blink-cmp-config-keymap for defining your own keymap
-	      keymap = { preset = 'enter' },
+		-- All presets have the following mappings:
+	      	-- C-space: Open menu or open docs if already open
+	      	-- C-n/C-p or Up/Down: Select next/previous item
+	      	-- C-e: Hide menu
+	      	-- C-k: Toggle signature help (if signature.enabled = true)
+	      	--
+	      	-- See :h blink-cmp-config-keymap for defining your own keymap
+	      	keymap = { preset = 'enter' },
 
-	      appearance = {
-	        nerd_font_variant = 'normal'
-	      },
+	      	appearance = {
+	      	  nerd_font_variant = 'normal'
+	      	},
 
-	      -- (Default) Only show the documentation popup when manually triggered
-	      completion = { documentation = { auto_show = false } },
+	      	-- (Default) Only show the documentation popup when manually triggered
+	      	completion = { documentation = { auto_show = false } },
 
-	      -- Default list of enabled providers defined so that you can extend it
-	      -- elsewhere in your config, without redefining it, due to `opts_extend`
-	      sources = {
-	        default = { 'lsp', 'path', 'snippets', 'buffer' },
-	      },
+	      	-- Default list of enabled providers defined so that you can extend it
+	      	-- elsewhere in your config, without redefining it, due to `opts_extend`
+	      	sources = {
+	      	  default = { 'lsp', 'path', 'snippets', 'buffer' },
+	      	},
 
-	      -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-	      -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-	      -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-	      --
-	      -- See the fuzzy documentation for more information
-	      fuzzy = { implementation = "prefer_rust_with_warning" }
+	      	-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+	      	-- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+	      	-- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+	      	--
+	      	-- See the fuzzy documentation for more information
+		fuzzy = { implementation = "prefer_rust_with_warning" },
+		signature = { enabled = true },
 	    },
 	    opts_extend = { "sources.default" }
 	},	
@@ -173,17 +180,26 @@ require("lazy").setup({
 	      vim.g.vimtex_view_method = "zathura"
 	    end
 	},
+
+	-- Markdown preview
+	-- (npm install -g yarn)
+	{
+	    "iamcco/markdown-preview.nvim",
+	    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+	    build = "cd app && yarn install",
+	    init = function()
+	      vim.g.mkdp_filetypes = { "markdown" }
+	    end,
+	    ft = { "markdown" },
+	},
 	
 	-- Edit files and directories like a buffer
 	{
 	    'stevearc/oil.nvim',
-	    opts = {
-		view_options = {
-		    show_hidden = true
-		}
-	    },
-	    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
 	    lazy = false,
+	    opts = {
+		view_options = { show_hidden = true }
+	    },
 	},
 
 	-- Adds support for LaTeX symbols for Julia
