d70936f21462a7cad4556924c92c48399d590075
Nicola Mignoni <>
2026-08-03 13:03:58 +0200

    Add support for Lean4

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -226,5 +226,22 @@ require("lazy").setup({
 
 	-- Adds support for LaTeX symbols for Julia
 	{ "JuliaEditorSupport/julia-vim" },
+
+	-- Lean support
+	{
+	    'Julian/lean.nvim',
+	    event = { 'BufReadPre *.lean', 'BufNewFile *.lean' },
+	    dependencies = {
+	      -- optional dependencies:
+	      -- 'nvim-telescope/telescope.nvim', -- for Lean-specific pickers
+	      -- 'andymass/vim-matchup',          -- for enhanced % motion behavior
+	      -- 'andrewradev/switch.vim',        -- for switch support
+	      -- 'tomtom/tcomment_vim',           -- for commenting
+	    },
+	    ---@type lean.Config
+		opts = { -- see the manual for full configuration options
+		mappings = true,
+	    }
+	}
     }
 })
